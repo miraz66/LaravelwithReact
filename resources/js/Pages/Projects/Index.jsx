598f56c278517b84003fe298afe35a908c3eb9a0
Mirajul Islam <mirajulislam0509@gmail.com>
2024-04-14 19:32:16 +0600

    Project Status

--- resources/js/Pages/Projects/Index.jsx
+++ resources/js/Pages/Projects/Index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Head, Link } from "@inertiajs/react";
+import Pagination from "@/Components/Pagination";
 
 export default function index({ auth, projects }) {
     return (
@@ -16,22 +17,24 @@ export default function index({ auth, projects }) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-4">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
+                        <div className="p-6 text-gray-600">
                             {/* <pre>{JSON.stringify(projects, undefined, 2)}</pre> */}
 
                             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:to-gray-400">
-                                <thead className="text-xs text-white uppercase bg-gray-50 dark:bg-gray-700 dark:to-gray-400 border-b-2 border-gray-500">
+                                <thead className="text-sm text-white uppercase bg-gray-50 dark:bg-gray-700 dark:to-gray-400 border-b-2 border-gray-500">
                                     <tr className="text-nowrap">
-                                        <th className="px-3 py-2">ID</th>
-                                        <th className="px-3 py-2">Image</th>
-                                        <th className="px-3 py-2">Name</th>
-                                        <th className="px-3 py-2">Status</th>
-                                        <th className="px-3 py-2">
+                                        <th className="px-3 py-4">ID</th>
+                                        <th className="px-3 py-4">Image</th>
+                                        <th className="px-3 py-4">Name</th>
+                                        <th className="px-3 py-4">Status</th>
+                                        <th className="px-3 py-4">
                                             Create Date
                                         </th>
-                                        <th className="px-3 py-2">Due Date</th>
-                                        <th className="px-3 py-2">Create By</th>
-                                        <th className="px-3 py-2">Actions</th>
+                                        <th className="px-3 py-4">Due Date</th>
+                                        <th className="px-3 py-4">Create By</th>
+                                        <th className="px-3 py-4 text-end">
+                                            Actions
+                                        </th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -44,7 +47,7 @@ export default function index({ auth, projects }) {
                                                 <img
                                                     src={project.image_path}
                                                     alt="fake image"
-                                                    style={{ width: 100 }}
+                                                    style={{ width: 120 }}
                                                 />
                                             </td>
                                             <td className="px-3 py-2">
@@ -67,11 +70,31 @@ export default function index({ auth, projects }) {
                                                 {project.updated_by.name}
                                             </td>
 
-                                            <td className="px-3 py-2"></td>
+                                            <td className="px-3 py-2">
+                                                <Link
+                                                    href={route(
+                                                        "project.edit",
+                                                        project.id
+                                                    )}
+                                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
+                                                >
+                                                    Edit
+                                                </Link>
+                                                <Link
+                                                    href={route(
+                                                        "project.destroy",
+                                                        project.id
+                                                    )}
+                                                    className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                                                >
+                                                    Delete
+                                                </Link>
+                                            </td>
                                         </tr>
                                     ))}
                                 </tbody>
                             </table>
+                            <Pagination links={projects.meta.links} />
                         </div>
                     </div>
                 </div>
