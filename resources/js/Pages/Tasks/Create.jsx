44d7f1d4fbb5a42c7b8b295671977aab38114ad5
Mirajul Islam <mirajulislam0509@gmail.com>
2024-05-01 09:24:12 +0600

    Task Create

--- resources/js/Pages/Tasks/Create.jsx
+++ resources/js/Pages/Tasks/Create.jsx
@@ -193,6 +193,7 @@ export default function Create({ auth }) {
                                     >
                                         Submit Submit Submit Submit Submit
                                         Submit Submit Submit Submit Submit
+                                        Submit Submit Submit Submit Submit
                                         Submit Submit Submit
                                     </SecondaryButton>
                                 </div>
