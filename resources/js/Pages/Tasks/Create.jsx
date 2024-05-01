de6f035905aab1d067295196cb49ed552f58ff03
Mirajul Islam <mirajulislam0509@gmail.com>
2024-05-01 22:25:55 +0600

    Task Create

--- resources/js/Pages/Tasks/Create.jsx
+++ resources/js/Pages/Tasks/Create.jsx
@@ -152,6 +152,62 @@ export default function Create({ auth }) {
                                     />
                                 </div>
 
+                                {/* Create a new Task from priority */}
+                                <div>
+                                    <InputLabel
+                                        htmlFor="task_project_id"
+                                        value="Project"
+                                    />
+
+                                    <SelectInput
+                                        id="task_project_id"
+                                        name="project_id"
+                                        value={data.project_id}
+                                        className="mt-1 block w-full py-2"
+                                        onChange={(e) =>
+                                            setData(
+                                                "project_id",
+                                                e.target.value
+                                            )
+                                        }
+                                    >
+                                        <option value="">Select Project</option>
+                                        <option value="">TODO</option>
+                                    </SelectInput>
+                                    <InputError
+                                        message={errors.project_id}
+                                        className="mt-2"
+                                    />
+                                </div>
+
+                                {/* Create a new Task from Assigned User */}
+                                <div>
+                                    <InputLabel
+                                        htmlFor="task_assigned_user"
+                                        value="Assigned User"
+                                    />
+
+                                    <SelectInput
+                                        name="assigned_user_id"
+                                        id="task_assigned_user"
+                                        value={data.assigned_user_id}
+                                        className="mt-1 block w-full py-2"
+                                        onChange={(e) =>
+                                            setData(
+                                                "assigned_user_id",
+                                                e.target.value
+                                            )
+                                        }
+                                    >
+                                        <option value="">Select User</option>
+                                        <option value="low">TODO</option>
+                                    </SelectInput>
+                                    <InputError
+                                        message={errors.assigned_user_id}
+                                        className="mt-2"
+                                    />
+                                </div>
+
                                 {/* Create a new Task from Description */}
                                 <div>
                                     <InputLabel
@@ -185,13 +241,13 @@ export default function Create({ auth }) {
                                 <div className="flex justify-end gap-5">
                                     <Link
                                         // href={route("task.index")}
-                                        className="inline-flex items-center px-8 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                                        className="inline-flex items-center px-8 py-3 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
                                     >
                                         Cancel
                                     </Link>
                                     <SecondaryButton
                                         type="submit"
-                                        className="bg-emerald-600 border-emerald-600 hover:bg-emerald-500 px-20 py-3"
+                                        className="bg-emerald-600 border-emerald-600 hover:bg-emerald-500 px-24"
                                     >
                                         Submit
                                     </SecondaryButton>
